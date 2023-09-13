d9fff739d33002662de1d526ef32c5531c1e850d
vika-tsymbaliyk <vika.bronytska@gmail.com>
2023-09-13 11:17:07 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { Searchbar } from "./Searchbar/Searchbar";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Button } from "./Button/Button";
@@ -8,102 +8,83 @@ import { Loader } from "./Loader/Loader";
 import { ModalWindow } from "./Modal/Modal";
 
 
-export class App extends Component {
-  state = {
-    query: '',
-    images: [],
-    page: 1,
-    loading: false,
-    error: false,
-    isVisible: false,
-    isEmpty: false,
-    showModal: false,
-    modalImg: {
-      url:'',
-      alt:''
-    },
-  };
+export const App =()=> {
+const [images, setImages] = useState([]);
+const [query, setQuery] = useState('');
+const [page, setPage] = useState(1);
+const [loading, setLoading] = useState(false);
+const [error, setError] = useState(false);
+const [isVisible, setIsVisible] = useState(false);
+const [isEmpty, setIsEmpty] = useState(false);
+const [showModal, setShowModal] = useState(false);
+const [modalImg, satModalImg]= useState({
+  url:'',
+  alt:''
+})
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.query !== this.state.query ||
-      prevState.page !== this.state.page
-    ) { this.fetchPhoto(this.state.query, this.state.page) }
+useEffect(()=>{fetchPhoto(query,page)},[query,page])
+
+const fetchPhoto = async(query, page)=>{
+  if (!query) {
+    return;
   }
-
-  fetchPhoto = async(query, page)=>{
-    if (!query) {
-      return;
+  setLoading(true);
+  try {
+    const {totalHits, hits} = await fetchPhotoByQ(query, page);
+    if (hits.length === 0) {
+      setIsEmpty(true);
     }
-    this.setState({ loading: true});
-    try {
-      
-      const {totalHits, hits} = await fetchPhotoByQ(query, page);
-      if (hits.length === 0) {
-        this.setState({ isEmpty: true });
-      }
-      this.setState(prevState => 
-        ({images: [...prevState.images, ...hits],
-          isVisible: page < Math.ceil(totalHits/itemsPerPage),
-        }));
-    } catch (error) {
-      this.setState({ error: error.message })
-    } finally {
-      this.setState({ loading: false });
-    }
+    setImages(prevImages => [...prevImages, ...hits]);
+    setIsVisible(page < Math.ceil(totalHits/itemsPerPage))
+    
+  } catch (error) {
+    setError(error.message)
+  } finally {
+    setLoading(false);
   }
+}
 
-  handleSubmit = (evt) => {
-    evt.preventDefault();
-    const serchWord = evt.target.elements.query.value.trim();
-    this.setState({
-      query: serchWord ,
-      images: [],
-      page: 1,
-      isEmpty: false,
-      error: null,
-    });
-  };
-  
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));    
-  };
+const handleSubmit = (evt) => {
+  evt.preventDefault();
+  const serchWord = evt.target.elements.query.value.trim();
+  setQuery(serchWord);
+  setImages([]);
+  setPage(1);
+  setIsEmpty(false);
+  setError(null);
+};
 
-  handleOpenModal = ()=> {
-    this.setState({ showModal: true });
-  }
-  showLargeImg = (largeImageURL, tags)=>{
-    this.setState({ modalImg:{
-      url: largeImageURL,
-      alt: tags,
-    } });
-  }
-  
-  handleCloseModal = ()=> {
-    this.setState({ showModal: false });
-  }
+const handleLoadMore = () => {
+  setPage(prevPage => prevPage.page + 1);    
+};
 
-  render() {
-    const { images, isVisible, isEmpty, loading, error, modalImg, showModal } = this.state;
+const handleOpenModal = ()=> {
+  setShowModal(true);
+}
+
+const showLargeImg = (largeImageURL, tags)=>{
+  satModalImg({ 
+    url: largeImageURL,
+    alt: tags,
+  } );
+}
+
+const handleCloseModal = ()=> {
+  setShowModal(false);
+}
 
     return (
       <Layout>
-        <ModalWindow isOpen={showModal} handleCloseModal={this.handleCloseModal} modalImg={modalImg}/>
+        <ModalWindow isOpen={showModal} handleCloseModal={handleCloseModal} modalImg={modalImg}/>
 
-        <Searchbar onSubmit={this.handleSubmit}/>
+        <Searchbar onSubmit={handleSubmit}/>
 
         {loading && <Loader/>}
         {error && (<p >❌ Something went wrong - {error}</p>)}
         {isEmpty && (<p >Sorry. There are no images ... 😭</p>)}
-        {images.length > 0 &&  (<ImageGallery images={images} onClick={this.handleOpenModal}  onShowModalImg={this.showLargeImg}/>)}
-        {isVisible &&  (<Button onClick={this.handleLoadMore}/>)}
-
-        
-
+        {images.length > 0 &&  (<ImageGallery images={images} onClick={handleOpenModal}  onShowModalImg={showLargeImg}/>)}
+        {isVisible &&  (<Button onClick={handleLoadMore}/>)}
 
         </Layout>
     );
-  }
 }
